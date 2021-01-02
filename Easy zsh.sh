0207b5596de6b14b19789bf9b75af4b54fd35d34
Batuhan Yaşar Yılmaz <bthyilmaz@outlook.com>
2021-01-02 16:39:16 +0300

    Create Easy zsh.sh

---
+++ Easy zsh.sh
@@ -0,0 +1,9 @@
+pacman -S zsh
+
+chsh -s /bin/zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+echo You need to install FuraMono manually. You can find in repository folder. 
