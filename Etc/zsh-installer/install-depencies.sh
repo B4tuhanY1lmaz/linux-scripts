3148b465a269efc078fb84b2a895daf172e7f978
Batuhan Yaşar Yılmaz <bthyilmaz@outlook.com>
2021-01-15 09:08:20 +0300

    Added more things to zsh-installer.
    Added more things to zsh-installer. Still not completed!

---
+++ Etc/zsh-installer/install-depencies.sh
@@ -0,0 +1,5 @@
+pacman -S zsh
+pacman -S yay
+pacman -S lsd
+
+echo All depencies are installed, now run the 'install-zsh.sh' file to install zsh.
---
+++ Etc/zsh-installer/install-zsh.sh
@@ -0,0 +1,7 @@
+chsh -s /bin/zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+echo You need to install FuraMono manually. You can find in repository folder. 
